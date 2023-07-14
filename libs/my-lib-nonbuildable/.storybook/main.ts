ad4ac405a413ca202531a4a0ca147ea73fab5de8
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-07-14 14:02:06 +0300

    install core-js and it works

--- libs/my-lib-nonbuildable/.storybook/main.ts
+++ libs/my-lib-nonbuildable/.storybook/main.ts
@@ -1,7 +1,7 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-  stories: ['../src/app/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  stories: ['../src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: ['@storybook/addon-essentials', '@nx/react/plugins/storybook'],
   framework: {
     name: '@storybook/react-webpack5',
